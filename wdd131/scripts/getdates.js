686c8758637ea6f284b10bbf6310bed682e2462f
murillodejesus <155997677+murillodejesus@users.noreply.github.com>
2024-10-15 21:11:24 -0300

    Delete wdd131 directory

--- wdd131/scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-// Dynamically populate the current year
-const yearElement = document.getElementById('currentyear');
-const currentYear = new Date().getFullYear();
-yearElement.textContent = currentYear;
-
-// Dynamically populate the last modified date
-const lastModifiedElement = document.getElementById('lastModified');
-lastModifiedElement.textContent = 'Last Modification: ' + document.lastModified;
